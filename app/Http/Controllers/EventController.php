4eb9a5f6dbc784fd7498bc7b3adb3cfeeabb3647
xlordt <xlordt@gmail.com>
2019-03-11 20:06:00 -0400

    Added CORS Middleware. Added list, add event routes. Added EventController. Added CORS to the kernel.

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events;
+use Illuminate\Http\Request;
+
+class EventController extends Controller  {
+
+
+    /**
+     * Get all events in the database.
+     * @return Events[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function all() {
+        return Events::all();
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function add(Request $request) {
+
+        $request->validate([
+            'event_abv'     => 'required|min:1,max:10',
+            'min_gpa'       => 'required',
+            'deadline'      => 'required|date_format:Y-m-d',
+            'test_schedule' => 'required|date',
+            'school_name'   => 'required|min:5,max:200',
+        ]);
+
+        $event = new Events();
+        $event->event_abv = $request->event_abv;
+        $event->min_gpa = $request->min_gpa;
+        $event->deadline = $request->deadline;
+        $event->test_schedule = $request->test_schedule;
+        $event->school_name = $request->school_name;
+
+        if ($event->save()) {
+
+            return response()->json(['success' => true], 200);
+        }
+
+        return response()->json(['success' => false], 409);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('fliers', 'FlierController@get');
+Route::post('events/add', 'EventController@add')->middleware('cors');
+Route::get('events/list', 'EventController@all');
