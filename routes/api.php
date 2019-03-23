90b3b93c10036463ad3fe7db6afd503b3172dd5d
xlord <xlordt@gmail.com>
2019-03-23 11:29:52 -0400

    test

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::post('events/add', 'EventController@add')->middleware('cors');
 Route::get('events/list', 'EventController@all');
