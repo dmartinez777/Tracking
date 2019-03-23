6e42a588842cd25301993c581ecac59c02e96104
xlord <xlordt@gmail.com>
2019-03-23 12:44:58 -0400

    Added interview model + migration script.

---
+++ database/migrations/2019_03_23_155026_create_interviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInterviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('interviews', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->bigInteger('process_id')->unsigned();
+            $table->boolean('has_scorecard')->default(0);
+            $table->boolean('is_active')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('interviews');
+    }
+}
