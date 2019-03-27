2230013f248593441a1d4e454b3e6d13db34010e
blitzo79 <F@Fe4q7r>
2019-03-27 23:18:31 +0700

    -new migration + models for Skill, Level, & AppSkill - missing relationships

---
+++ database/migrations/2019_03_27_160816_create_skills_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->integer('order')->unsigned();
+            $table->integer('group')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skills');
+    }
+}
---
+++ database/migrations/2019_03_27_161116_create_apps_skills_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apps_skills', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('apps_id')->unsigned();
+            $table->bigInteger('skills_id')->unsigned();
+            $table->bigInteger('levels_id')->unsigned();
+            $table->foreign('apps_id')->references('id')->on('apps');
+            $table->foreign('skills_id')->references('id')->on('skills');
+            $table->foreign('levels_id')->references('id')->on('levels');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apps_skills');
+    }
+}
