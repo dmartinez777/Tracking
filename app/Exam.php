6e2906c65a6636298bf07bf7b37293a84d6330e7
blitzo79 <F@Fe4q7r>
2019-03-28 04:36:08 +0700

    -fixed Exam model

--- app/Exam.php
+++ app/Exam.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Exam extends Model
 {
-    //
+    protected $table = "exams";
+    protected $fillable = array('name', 'max_score', 'max_duration', 'order', 'processes_id', 'is_active');
+
+
+
 }
